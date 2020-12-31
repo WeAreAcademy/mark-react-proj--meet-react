77e7ec2005763f5753bcaa8222e07bbd00bb9976
Richard Ng <richardcrng@gmail.com>
2020-12-31 11:24:01 +0000

    Demonstrate reusing variables

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const adjective = "exciting";
+const noun = "wood";
+const verb = "chuck";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  return (
+    <div>
+      <h1>Hello world from React!</h1>
+      <p>Did I obliterate something?</p>
+      <p>
+        But I get to do <b>{adjective} things</b> when I use a React app!
+      </p>
+      <p>
+        Like: how much {noun} could a {noun}
+        {verb} {verb} if a {noun}
+        {verb} could {verb} {noun}?
+      </p>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("arbitrarily-named-target"));
